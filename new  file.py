a4f06f10c7791d00f7175348c0f3c28f5a96cec1
Ahmed Mohamed <hamada55990@gmail.com>
2026-02-17 03:16:00 +0200

    first commit

---
+++ new  file.py
@@ -0,0 +1,28 @@
+
+# simple program to calculate days from yours
+print("----age in Days Calculator-----")
+age_years = int(input("inter your age in years:"))
+age_days = age_years*365
+print(f"you have lived for {age_days} days")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
